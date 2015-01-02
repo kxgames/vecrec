df264c490f8600c5047db328c9388c1d07d4cbd5
Kale Kundert <kale@thekunderts.net>
2015-01-02 13:19:32 -0800

    Add finalexam and coverage as dependencies.

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ distutils.core.setup(
         name='vecrec',
         version=version,
         author='Kale Kundert and Alex Mitchell',
-        packages=['vecrec'],
         url='https://github.com/kxgames/vecrec',
         download_url='https://github.com/kxgames/vecrec/tarball/'+version,
         license='LICENSE.txt',
         description="A 2D vector and rectangle library.",
         long_description=open('README.rst').read(),
-        keywords=['2D', 'vector', 'rectangle', 'library'])
+        keywords=['2D', 'vector', 'rectangle', 'library'],
+        packages=['vecrec'],
+        requires=['finalexam', 'coverage'],
+)
